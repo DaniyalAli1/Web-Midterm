0c1220faaafad5cbfb73712b01bb59e0e02822ed
CSLab Administrator <cslabadmin@shu.edu.pk>
2024-04-24 10:59:05 +0500

    first commit

---
+++ app/components/navbar.tsx
@@ -0,0 +1,14 @@
+import Nav1 from "../../public/asset/Nav1.svg"
+
+export function Navbar() {
+
+    <p>ali</p>
+
+}
+
+
+// {/* <nav>
+//         <div>
+//             <img src="Nav1" alt="" />
+//         </div>
+//     </nav> */}
